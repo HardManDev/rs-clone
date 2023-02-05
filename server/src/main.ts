69c1dedb84b4a685a894fe048d03b0c17095e9d8
HardManDev <hardman.dev@protonmail.com>
2023-02-05 12:14:47 +0300

    refactor: apply ESLint rules

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import AppModule from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
 }
 
-(async () => {
+(async (): Promise<void> => {
   await bootstrap();
 })();
