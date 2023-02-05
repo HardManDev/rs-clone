014b34cb96f178c0bb37f95aacda22baf14c8b0d
HardManDev <hardman.dev@protonmail.com>
2023-02-05 12:05:35 +0300

    feat: create mock API test

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,1 +1,15 @@
 import '@styles/main';
+
+(
+  async (): Promise<void> => {
+    const text = await fetch(
+      `${process.env.API_URL}`,
+      {
+        method: 'GET',
+      },
+    )
+      .then((res) => res.text());
+
+    console.log(text);
+  }
+)();
