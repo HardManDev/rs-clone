d1e060f83a01320fd35e78c285bc115661e396a6
jukfloyd <zhuchkovav@gmail.com>
2023-02-11 23:58:36 +0300

    feat: add player animation

--- client/src/scripts/components/dave.ts
+++ client/src/scripts/components/dave.ts
@@ -5,13 +5,15 @@ import {
 import {
   DaveLook, DaveMove, DaveShoot, DaveState,
 } from '../../types/dave';
+import PlayAnimator from '../controllers/playAnimator';
+import Direction from '../../types/enums/directions';
 
 class Player {
   x = 0;
 
   y = 0;
 
-  w = 64;
+  w = 72;
 
   h = 96;
 
@@ -35,6 +37,8 @@ class Player {
 
   lives = 3;
 
+  animation: PlayAnimator;
+
   constructor(leftFeet: LeftFeet) {
     this.sprite.classList.add('player');
     this.x = leftFeet.x;
@@ -42,29 +46,68 @@ class Player {
     this.sprite.style.width = `${this.w}px`;
     this.sprite.style.height = `${this.h}px`;
     this.setPosition();
+    this.animation = new PlayAnimator(this.sprite);
   }
 
   setView(): void {
     if (this.state === DaveState.FALLING) {
-      if (this.look === DaveLook.LEFT) {
-        this.sprite.innerHTML = '<svg fill="#000000" height="100px" width="60px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 347.341 347.341" xml:space="preserve"><polygon points="347.34,21.213 326.127,0 30,296.128 30,247.487 0,247.487 0,347.341 99.854,347.34 99.854,317.34 51.214,317.34 "/></svg>';
-      } else if (this.look === DaveLook.RIGHT) {
-        this.sprite.innerHTML = '<svg fill="#000000" height="100px" width="60px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 347.341 347.341" xml:space="preserve"><polygon points="247.487,347.341 347.341,347.34 347.34,247.487 317.34,247.487 317.34,296.127 21.213,0 0,21.213 296.127,317.34 247.487,317.341 "/></svg>';
-      }
+      this.animation.fall(
+        this.look === DaveLook.LEFT
+          ? Direction.RIGHT
+          : Direction.LEFT,
+      );
     } else if (this.state === DaveState.JUMPING_UP) {
-      if (this.look === DaveLook.LEFT) {
-        this.sprite.innerHTML = '<svg fill="#000000" height="100px" width="60px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 347.341 347.341" xml:space="preserve"><polygon points="347.34,326.127 51.213,30 184.706,30 184.706,0 0,0 0,184.706 30,184.706 30,51.213 326.127,347.341 "/></svg>';
-      } else if (this.look === DaveLook.RIGHT) {
-        this.sprite.innerHTML = '<svg fill="#000000" height="100px" width="60px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 347.341 347.341" xml:space="preserve"><polygon points="347.341,107.783 347.339,0 239.559,0.002 282.843,43.285 0,326.128 21.213,347.341 304.056,64.498 "/></svg>';
+      this.animation.jump(
+        this.look === DaveLook.LEFT
+          ? Direction.RIGHT
+          : Direction.LEFT,
+      );
+    } else if (this.state === DaveState.SHOOTING) {
+      if (this.shoot === DaveShoot.UP) {
+        this.animation.shoot(
+          this.look === DaveLook.LEFT
+            ? Direction.TOP_LEFT
+            : Direction.TOP_RIGHT,
+        );
+      } else if (this.shoot === DaveShoot.DOWN) {
+        this.animation.shoot(
+          this.look === DaveLook.LEFT
+            ? Direction.DOWN_LEFT
+            : Direction.DOWN_RIGHT,
+        );
+      } else {
+        this.animation.shoot(
+          this.look === DaveLook.LEFT
+            ? Direction.LEFT
+            : Direction.RIGHT,
+        );
       }
-    } else if (this.shoot === DaveShoot.UP) {
-      this.sprite.innerHTML = 'Shooting UP';
-    } else if (this.shoot === DaveShoot.DOWN) {
-      this.sprite.innerHTML = 'Shooting DOWN';
-    } else if (this.look === DaveLook.LEFT) {
-      this.sprite.innerHTML = '<svg fill="#000000" height="100px" width="60px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 476.213 476.213" xml:space="preserve"><polygon points="476.213,223.107 76.212,223.107 76.212,161.893 0,238.108 76.212,314.32 76.212,253.107 476.213,253.107 "/></svg>';
-    } else if (this.look === DaveLook.RIGHT) {
-      this.sprite.innerHTML = '<svg fill="#000000" height="100px" width="60px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 476.213 476.213" xml:space="preserve"><polygon points="476.213,238.105 400,161.893 400,223.106 0,223.106 0,253.106 400,253.106 400,314.32 "/></svg>';
+    } else if (this.state === DaveState.RUNNING) {
+      this.animation.move(
+        this.look === DaveLook.LEFT
+          ? Direction.LEFT
+          : Direction.RIGHT,
+      );
+    } else if (this.state === DaveState.STANDING) {
+      if (this.shoot === DaveShoot.UP) {
+        this.animation.look(
+          this.look === DaveLook.LEFT
+            ? Direction.TOP_LEFT
+            : Direction.TOP_RIGHT,
+        );
+      } else if (this.shoot === DaveShoot.DOWN) {
+        this.animation.look(
+          this.look === DaveLook.LEFT
+            ? Direction.DOWN_LEFT
+            : Direction.DOWN_RIGHT,
+        );
+      } else {
+        this.animation.look(
+          this.look === DaveLook.LEFT
+            ? Direction.LEFT
+            : Direction.RIGHT,
+        );
+      }
     }
   }
 
