27e0e1c48490af93bb2c294816a457f7947d2fd4
VasaSkor <106305964+VasaSkor@users.noreply.github.com>
2023-02-11 18:17:10 +0300

    feat: add an interface for select the desired animation

---
+++ client/src/types/interfaces/playerAnimator.ts
@@ -0,0 +1,12 @@
+import Direction from '../enums/directions';
+
+export interface IPlayerAnimator {
+  stand(direction: Direction): void
+  move(direction: Direction): void
+  jump(direction: Direction): void
+  fall(direction: Direction): void
+  reload(): void
+  shoot(direction: Direction): void
+  look(direction: Direction): void
+  exit(): void
+}
