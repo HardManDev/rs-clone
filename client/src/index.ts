bf18917d99e11fef6d995fff2b2f5acb748a07c7
VasaSkor <106305964+VasaSkor@users.noreply.github.com>
2023-02-11 18:24:00 +0300

    feat: add animation styles and its keyframes

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,19 +1,6 @@
 import '@styles/main';
+import '@styles/playerAnimation';
 import PlayLevel from './scripts/controllers/playLevel';
 
 const playLevel = new PlayLevel();
 playLevel.startGame();
-
-(
-  async (): Promise<void> => {
-    const text = await fetch(
-      `${process.env.API_URL}`,
-      {
-        method: 'GET',
-      },
-    )
-      .then((res) => res.text());
-
-    console.log(text);
-  }
-)();
