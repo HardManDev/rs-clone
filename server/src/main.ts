3b68c2acb3803cac97b2675cf696a9a2e61dd7a5
HardManDev <hardman.dev@protonmail.com>
2023-02-12 17:00:06 +0300

    refactor: bootstrap function

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as process from 'process';
+import { AppModule } from './modules/app/app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
   await app.listen(process.env.PORT || 3000);
 }
 
