0b31999cbb8735f4a046f3010fc333faf95b6243
HardManDev <hardman.dev@protonmail.com>
2023-02-27 21:00:54 +0300

    hotfix: fix remove cookie logic

--- client/src/scripts/utils/deleteCookie.ts
+++ client/src/scripts/utils/deleteCookie.ts
@@ -4,5 +4,5 @@ export default function deleteCookie(name: string): void {
     .match(originRegex);
 
   // document.cookie = `${name}=;domain=${origin ? origin[1] : 'localhost'};expires=Thu, 01 Jan 1970 00:00:00 GMT;Path=/;Secure;SameSite=None;`;
-  document.cookie = 'auth_token= ;expires=Fri, 05 Jun 2020 12:00:00 UTC;domain=hardmandev-rs-clone.up.railway.app;Path=/;Secure;SameSite=None;';
+  document.cookie = 'auth_token= ;expires=Fri, 05 Jun 2024 12:00:00 UTC;domain=hardmandev-rs-clone.up.railway.app;Path=/;Secure;SameSite=None;';
 }
