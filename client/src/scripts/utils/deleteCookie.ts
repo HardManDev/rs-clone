c36598cec5120b2f686209d464807ddc7bcf07c4
HardManDev <hardman.dev@protonmail.com>
2023-02-27 20:25:49 +0300

    hotfix: fix remove cookie logic

--- client/src/scripts/utils/deleteCookie.ts
+++ client/src/scripts/utils/deleteCookie.ts
@@ -3,5 +3,6 @@ export default function deleteCookie(name: string): void {
   const origin = (process.env.API_URL || 'http://localhost:3000')
     .match(originRegex);
 
+  console.log(origin ? origin[1] : 'localhost');
   document.cookie = `${name}=;domain=${origin ? origin[1] : 'localhost'};expires=${Date.now()}; Path=/; Secure; SameSite=None;`;
 }
