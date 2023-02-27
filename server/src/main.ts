e87f4da1cab5c9a7e17a520321cfb28ed8fb455e
HardManDev <hardman.dev@protonmail.com>
2023-02-27 05:20:51 +0300

    hotfix: fix CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,9 +5,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
+  console.log(`CORS: ${process.env.FRONT_END_URL || 'http://localhost:8080'}`)
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:8080', '*', (process.env.FRONT_END_URL || '*')],
+    origin: [(process.env.FRONT_END_URL || 'http://localhost:8080')],
     credentials: true,
   });
 
