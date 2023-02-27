c6f90b2f989fd055f7318d7971087527ef883878
HardManDev <hardman.dev@protonmail.com>
2023-02-27 23:39:15 +0300

    hotfix: fix cookies policy

--- client/src/scripts/utils/deleteCookie.ts
+++ client/src/scripts/utils/deleteCookie.ts
@@ -1,6 +1,6 @@
 export default function deleteCookie(name: string): void {
   const originRegex = /^(?:(?:[a-z]+:)?\/\/)?(?:[^@/]+@)?([^:/]+)/i;
-  const origin = (process.env.API_URL || 'http://localhost:3000')
+  const origin = (process.env.DOMAIN || 'http://localhost:8080')
     .match(originRegex);
 
   document.cookie = `${name}=;domain=${origin ? origin[1] : 'localhost'};expires=Thu, 01 Jan 1970 00:00:00 GMT;Path=/;Secure;SameSite=None;`;
